82f0c781a9e2353d7e58408994831fc4d1771ac2
Vin2315 <viniciovillalta@gmail.com>
2023-05-15 13:55:39 -0500

    Formulaire avec send problemme

--- 04-formulaires/src/Composants/FormulaireComposant.js
+++ 04-formulaires/src/Composants/FormulaireComposant.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export const FormulaireComposant = () => {
 
-    // para que aparesca en panatalla//luego el setUser(user); luego el Div 
+            // para que aparesca en panatalla//luego el setUser(user); luego el Div lo declaro al inicio de la pagina  
     const [user, setUser] = useState ({});
-    // para que aparesca los datos del usuario 
-    
+            
+        // para que aparesca los datos del usuario 
     const LoadDataForms = e =>{
         e.preventDefault();
 
@@ -13,41 +13,61 @@ export const FormulaireComposant = () => {
         let user ={
             name: data.name.value,
             lastname:data.lastname.value,
-            sex:data.sex.value,
-            biblio:data.biblio.value
+            genero:data.genero.value,
+            biblio:data.biblio.value,
+            send:data.sent.value
         };
 
         console.log(user);
         setUser(user);
     }
 
+    const changeData = e =>{
+        let name_del_input = e.target.name;
+        let name_pour_modifie = user;
+
+        setUser(estado_previo => ({
+             ...estado_previo, 
+             [name_del_input]: e.target.value
+            }) 
+        );
+    }
     return (
         <div>
             <h1>Formulaires avec React</h1>
-
-            <div className='info_user' aria-label='' >
-                {user.name} {user.lastname} son sex est {user.sex} et sa Biographie est <p>{user.biblio}</p>
-            </div>
-
+            
+            {/* //para que se muestre la caja si todo escribe en la caja y muestra los valore por */}
+            { user.biblio && user.biblio.length >= 1 && 
+                (
+                    <div className='info_user label label-gray' >
+                        {user.name} {user.lastname} son genero est {user.genero} et sa Biographie est <p>{user.biblio}</p>
+                    </div>
+                )
+            }
+            {/* //onsubmit esta ejecuta una opcion cuando envie el formulario */}
             <form onSubmit={LoadDataForms}>
                 <input type ="text" 
                        placeholder='name' 
-                       name="name">
+                       name="name"
+                       onChange={changeData}>
                 </input>
                 
                 <input type ="text" 
                         placeholder='lastname' 
-                        name="lastname">
-
+                        name="lastname"
+                        onChange={changeData}>
                 </input>
                 
-                <select name="sex">
+                <select name="genero"  onChange={changeData}>
                     <option value="homme" >Homme</option>
                     <option value="femme" >Femme</option>    
-                </select>
-                <textarea placeholder='bibliography' name="biblio"></textarea>
 
-                <input type ="submit" value="Send" ></input>
+                </select>
+                <textarea placeholder='bibliography' 
+                name="biblio"
+                onChange={changeData}></textarea>
+
+                <input type ="submit" value="Send" name="Send"></input>
                 
 
             </form>            
@@ -57,4 +77,8 @@ export const FormulaireComposant = () => {
     )
 }
 
-export default FormulaireComposant
+
+// onchange permite cer como se cambio la info en las pantalla 
+
+
+export default FormulaireComposant;
